8a7822a337ba81b495549fba65f0cd7f481e05e9
Sasaki Kazuhiro <sasaki.kazuhiro@classmethod.jp>
2022-01-07 23:58:32 +0900

    fix for lint

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,9 @@
 rootProject.name = "idea envfile plugin"
 pluginManagement {
     repositories {
-        maven(url="https://oss.sonatype.org/content/repositories/releases/")
-        maven(url="https://www.jetbrains.com/intellij-repository/releases/")
-        maven(url="https://plugins.gradle.org/m2/")
+        maven(url = "https://oss.sonatype.org/content/repositories/releases/")
+        maven(url = "https://www.jetbrains.com/intellij-repository/releases/")
+        maven(url = "https://plugins.gradle.org/m2/")
         gradlePluginPortal()
     }
 }
